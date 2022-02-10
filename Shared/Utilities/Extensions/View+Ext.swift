4f8f040c8623896c700cdefb96524a6c65283722
Noe Duran <durann031@gmail.com>
2022-02-09 21:38:10 -0500

    deleted file oop

--- Shared/Utilities/Extensions/View+Ext.swift
+++ Shared/Utilities/Extensions/View+Ext.swift
@@ -4,11 +4,21 @@
 //
 //  Created by Noé Duran on 1/11/22.
 //
-
 import SwiftUI
 
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
